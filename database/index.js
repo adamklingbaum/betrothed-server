30dc2e755085b3a5810c9283c540d71e413a3334
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-10 19:00:45 -0500

    rename dbConnection to db

--- database/index.js
+++ database/index.js
@@ -11,6 +11,6 @@ try {
   console.log('Could not connect');
 }
 
-const dbConnection = mongoose.connection;
-dbConnection.on('error', (err) => console.log(`Connection error ${err}`));
-dbConnection.once('open', () => console.log('Connected to DB!'));
+const db = mongoose.connection;
+db.on('error', (err) => console.log(`Connection error ${err}`));
+db.once('open', () => console.log('Connected to DB!'));
--- models/event.js
+++ models/event.js
@@ -1,3 +1,1 @@
-module.exports = {
-  get: () => {},
-};
+const mongoose = require('mongoose');
