4f84613e17f945e49315c139db92632fae86be8a
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-13 21:19:41 -0500

    add static file middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use('/api', router);
 
 module.exports = app;
